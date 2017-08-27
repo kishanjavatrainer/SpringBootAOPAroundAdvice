024f80bb437a57701f3283ebc44f1827daef1278
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-08-27 15:18:29 +0530

    code

---
+++ SpringBootAOPAroundAdvice/src/main/java/com/infotech/SpringBootAopApplication.java
@@ -0,0 +1,20 @@
+package com.infotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.infotech.model.Account;
+import com.infotech.service.impl.AccountServiceImpl;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass=true)
+public class SpringBootAopApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(SpringBootAopApplication.class, args);
+		AccountServiceImpl accountService = ctx.getBean("accountService", AccountServiceImpl.class);
+		accountService.updateAccountBalance(new Account("6778888886", "Money tranfer"), 3000L);
+	}
+}
